359fbe4f4902176f1ffc76ea32a0c0771f375ead
Aliaksandr Prokopenia <prokopenya_a@mail.ru>
2021-08-29 22:47:57 +0300

    add base pattern, home page

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Post
 
-# Create your tests here.
+
+class BlogTests(TestCase):
+
+    def setUp(self) -> None:
+        self.user = get_user_model().objects.create_user(username='User_1', email='user@gmail.com', password='user')
+        self.post = Post.create(title='Test', text='some text', body='somebody', author=self.user)
+
+    def test_post_content(self):
+        self.assertEqual(str(self.post.title), 'Test')
+        self.assertEqual(str(self.post.body), 'somebody')
+
+    def test_post_list_view(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, "nice")
+        self.assertTemplateUsed(resp, 'home.html')
+
+    def test_post_detail(self):
+        resp = self.client.get('/post/1/')
+        no_resp = self.client.get('/post/100/')
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(no_resp.status_code, 400)
+
+        self.assertTemplateUsed(resp, 'post_detail.html')
--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,8 @@
-from .views import BlogListView
+from .views import BlogListView, BlogDetailView
 from django.urls import path, include
 from django.contrib import admin
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
-
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
 ]
